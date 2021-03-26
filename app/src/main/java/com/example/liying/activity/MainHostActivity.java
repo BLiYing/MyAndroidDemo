15b9724eaca8173e9b35fb4618f3755b39df91f1
BLiYing <872904178@qq.com>
2021-03-26 15:55:49 +0800

    测试

--- app/src/main/java/com/example/liying/activity/MainHostActivity.java
+++ app/src/main/java/com/example/liying/activity/MainHostActivity.java
@@ -1,7 +1,10 @@
 package com.example.liying.activity;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -17,6 +20,10 @@ import android.widget.Toast;
 
 import com.example.utils.NotificationBuildUtil;
 import com.example.view.MyView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -129,6 +136,7 @@ public class MainHostActivity extends AppCompatActivity {
                 sendShareMessage();
                 break;
             case R.id.main11:
+                invokeCetc15Map(this,null);
                 /*if (isConnect) {
                     intent.setClass(this, SubConversationListActivity.class);
                     startActivity(intent);
@@ -151,7 +159,7 @@ public class MainHostActivity extends AppCompatActivity {
 
     //发送分享的信息
     private void sendShareMessage() {
-        String packageName = "com.shiketongxun.rongxin.lite";
+        String packageName = "com.yuntongxun.rongxin.lite";
         Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);//主要用于判断用户是否安装了要分享过去的APP
         if (intent != null) {
             Intent share = new Intent();
@@ -160,11 +168,89 @@ public class MainHostActivity extends AppCompatActivity {
             share.setAction(Intent.ACTION_SEND);
             share.setType("text/plain"); //分享的是文本类型
             share.putExtra(Intent.EXTRA_TEXT, "https://blog.csdn.net/oudetu/article/details/78443826");//分享出去的内容
+//            share.putExtra(Intent.EXTRA_TEXT, "https://123.45");//分享出去的内容
+//            share.putExtra(Intent.EXTRA_TEXT, "http://192.168.1.217/page/news_detail.html?id=4731");//分享出去的内容
 //            startActivity(Intent.createChooser(share, ""));
             startActivity(share);
             getSupportFragmentManager().popBackStack();
         } else {
             Toast.makeText(getApplicationContext(), "你还没安装该应用，请先安装", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void invokeCetc15Map(Context context, Map arg){
+        final List<String> packages = checkInstalledPackage(MAP_PACKAGES);
+        if (packages.size() == 0) {
+            Toast.makeText(this,"安装地图app", Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            String appName = getProgramNameByPackageName(context,packages.get(0));
+            Toast.makeText(this,appName, Toast.LENGTH_SHORT).show();
+        }
+        Intent intent = new Intent();
+        intent.setAction("eterramap.app");
+        intent.putExtra("lon",(Double) 116.368);
+        intent.putExtra("lat",(Double) 39.9871);
+        intent.putExtra("name","太极大厦");
+        context.startActivity(intent);
+    }
+    //1.百度地图包名
+    public static final String BAIDUMAP_PACKAGENAME = "com.baidu.BaiduMap";
+    //2.高德地图包名
+    public static final String AUTONAVI_PACKAGENAME = "com.autonavi.minimap";
+    //3.腾讯地图包名
+    public static final String QQMAP_PACKAGENAME = "com.tencent.map";
+    //3.Google地图包名
+    public static final String GOOGLEMAP_PACKAGENAME = "com.google.android.apps.maps";
+    public static final String CETC15_PACKAGENAME = "com.cetc15.eterramap";
+    private static final String[] MAP_PACKAGES = {BAIDUMAP_PACKAGENAME, AUTONAVI_PACKAGENAME, QQMAP_PACKAGENAME, GOOGLEMAP_PACKAGENAME,CETC15_PACKAGENAME};
+
+
+    /**
+     * 检查手机上是否安装了指定的软件
+     *
+     * @param packageNames 可变参数 String[]
+     * @return 目标软件中已安装的列表
+     */
+    public List<String> checkInstalledPackage(String... packageNames) {
+
+        //获取packageManager
+        final PackageManager packageManager = getPackageManager();
+        //获取所有已安装程序的包信息
+        List<PackageInfo> packageInfos = packageManager.getInstalledPackages(0);
+        //用于存储
+        List<String> newPackageNames = new ArrayList<>();
+        int count = packageNames.length;
+
+        if (packageInfos != null && packageInfos.size() > 0) {
+
+            outermost:
+            for (String packageName : packageNames) {
+                for (int i = 0; i < packageInfos.size(); i++) {
+                    String packageInfo = packageInfos.get(i).packageName;
+                    if (packageInfo.contains(packageName)) {
+                        newPackageNames.add(packageName);
+                        if (newPackageNames.size() == count) {
+                            break outermost;//这里使用了循环标记，跳出外层循环
+                        }
+                    }
+                }
+            }
+        }
+        //判断packageNames中是否有目标程序的包名，有TRUE，没有FALSE
+        return newPackageNames;
+    }
+
+    public String getProgramNameByPackageName(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        String name = null;
+        try {
+            name = pm.getApplicationLabel(
+                    pm.getApplicationInfo(packageName,
+                            PackageManager.GET_META_DATA)).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return name;
     }
 }
